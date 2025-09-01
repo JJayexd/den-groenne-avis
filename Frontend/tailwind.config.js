54cc0e1151d8d2d456283e62604e83f21484d843
JJayexd <kontakt.emilbj@gmail.com>
2025-09-01 09:35:33 +0200

    tailwind.config.js oprettet for farve pallet

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          brand: {
+            dark: "#1D8439", // Mørk grøn
+            light: "#00A676", // Lys grøn
+            grey: "#F5F5F5", // Grå
+            white: "#FFFFFF", // Hvid
+            black: "#000000", // Sort
+          },
+          accent: "#952E0D", // Orange accent
+        },
+      },
+    },
+    plugins: [],
+  }
+  
